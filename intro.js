2b14c90a489299b1a4435c92b6e58134d28f48fe
Adobyzer <adam.abed02@hotmail.com>
2023-11-03 17:28:58 -0400

    Fetch object in js add new object and delete it witch checking hasOwnProperty()

--- intro.js
+++ intro.js
@@ -1151,3 +1151,86 @@ function checkObj(obj, checkProp) {
   }
   
   console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"));
+
+
+  //Manipulating Complex Objects
+
+/*
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+*/
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist" :"Bill",
+    "title" : "Help me in Complex world",
+    "release_year" : 2023,
+    "formats" :["mp4", 'mp3' ,'ova']
+  }
+];
+
+
+//Accesing nested object easy
+
+/*
+Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+*/
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+
+console.log(gloveBoxContents);
+
+
+
+//accesing nested arrays
+
+/*
+Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
+*/
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+//verification
+
+console.log(myPlants[1].list[1]);
