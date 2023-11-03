f4bc787f77796c052f21d602fc89081f393e1ecb
Adobyzer <adam.abed02@hotmail.com>
2023-11-03 03:09:09 -0400

    If case chain if else case and switch case utility big problem on it

--- intro.js
+++ intro.js
@@ -469,4 +469,532 @@ function processArg(num) {
 }
 
 processed = processArg(7);
-// Only change code below this line
+
+
+//file fifo
+function nextInLine(arr, item) {
+
+  /*
+  Add the number to the end of the array, then remove the first element of the array not the last AHAHAH so no pop.
+
+The nextInLine function should then return the element that was removed.
+
+*/
+
+arr.push(item);
+
+item = arr.shift();
+
+  return item;
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+
+function welcomeToBooleans() {
+  // return true instead of false by default
+
+  return true; // Change this line
+
+}
+
+// == (test value) but === (test type variable)
+
+/*
+Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+*/
+
+function testEqual(val) {
+  if (val == 12) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+// === strict equality doesnt do the convert of type so must be same value and same type
+
+/*
+Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
+*/
+
+function testStrict(val) {
+  if (val===7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+// to check the type of it use the "typeof" operator
+
+/*
+The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns the string Equal only when the values are strictly equal.
+*/
+
+function compareEquality(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+// Comparison with the Inequality Operator
+
+/*Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.
+*/
+
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+
+// Comparison with the Strict Inequality Operator (!==)
+
+function testStrictNotEqual(val) {
+  if (val!==17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+
+//Comparison with the Greater Than Operator >
+
+/*Add the greater than operator to the indicated lines so that the return statements make sense. according to return
+*/
+function testGreaterThan(val) {
+  if (val>100) {  
+    return "Over 100";
+  }
+
+  if (val>10) { 
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+
+//Comparison with the Greater Than Or Equal To Operator >=
+
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+
+//Comparison with the Less Than Operator
+
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+
+//Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+//Comparisons with the Logical And Operator &&
+
+/*
+Replace the two if statements with one statement, using the && operator, which will return the string Yes if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return the string No.
+*/
+function testLogicalAnd(val) {
+  
+  if(val<=50 && val>=25){
+    return "Yes"
+  }
+
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
+//Comparisons with the Logical Or Operator
+
+/*
+Combine the two if statements into one statement which returns the string Outside if val is not between 10 and 20, inclusive. Otherwise, return the string Inside.
+*/
+function testLogicalOr(val) {
+
+  if(val<10 || val>20){
+    return "Outside";
+  }
+  
+  
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
+
+
+  //Introducing Else Statements
+
+//Combine the if statements into a single if/else statement.
+
+function testElse(val) {
+  let result = "";
+  
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else{
+    result = "5 or Smaller";
+  }
+
+  return result;
+}
+
+testElse(4);
+
+
+//Introducing Else If Statements
+//Convert the logic to use else if statements.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  else {
+    return "Between 5 and 10";
+  }
+  
+}
+
+testElseIf(7);
+
+
+//logic from up to down so watch out
+//Change the order of logic in the function so that it will return the correct statements in all cases.
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+
+/*
+Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge
+*/
+
+function testSize(num) {
+  if(num<5){
+    return "Tiny"
+  }
+  else if(num<10){
+    return "Small"
+  }
+  else if(num<15){
+    return "Medium"
+  }
+  else if(num<20){
+    return "Large"
+  }
+  else{
+    return "Huge"
+  }
+ }
+ 
+ testSize(7);
+
+
+
+ const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+/*create this logic :
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+*/
+function golfScore(par, strokes) {
+  if(strokes == 1){
+    return names[0];
+  }
+  else if(strokes <= par-2){
+    return names[1];
+  }
+  else if(strokes == par-1){
+    return names[2];
+  }
+  else if(strokes == par){
+    return names[3];
+  }
+  else if(strokes == par+1){
+    return names[4];
+  }
+  else if(strokes == par+2){
+    return names[5];
+  }
+  else{
+    return names[6];
+  }
+
+}
+
+golfScore(5, 4);
+
+//selecting from Many Options with Switch Statements
+
+/*
+Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta
+*/
+
+function caseInSwitch(val) {
+  let answer = "";
+switch(val){
+  case 1: 
+  answer ="alpha";
+  break;
+
+  case 2:
+  answer ="beta";
+  break;
+
+  case 3:
+  answer ="gamma";
+  break;
+
+  case 4:
+  answer ="delta";
+  break;
+}
+return answer;
+}
+
+caseInSwitch(1);
+
+
+//add default option to switch statement
+/*
+a - apple
+b - bird
+c - cat
+default - stuff
+*/
+
+function switchOfStuff(val) {
+  let answer = "";
+  switch(val){
+    case "a" : answer = "apple";
+    break;
+
+    case "b" : answer = "bird";
+    break;
+
+    case "c" : answer = "cat";
+    break;
+
+    default : answer = "stuff";
+  }
+  
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+//use of break line
+
+/*
+Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+*/
+
+function sequentialSizes(val) {
+  let answer = "";
+
+  switch(val){
+    case 1:
+    case 2:
+    case 3: answer="Low";
+    break;
+    case 4:
+    case 5:
+    case 6: answer ="Mid";
+    break;
+    case 7:
+    case 8:
+    case 9: answer ="High";
+  }
+ 
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+//Replacing If Else Chains with Switch to code redability
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch(val){
+  case "bob" :  answer = "Marley";
+  break;
+  case 42 : answer = "The Answer";
+  break;
+  case 1 : answer ="There is no #1";
+  break;
+  case 99 : answer = "Missed me by this much!";
+  break;
+  case 7: answer ="Ate Nine"; 
+}
+
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+//simplify like we know boolean value and return will be either true or false
+function isLess(a, b) {
+  
+  // Only change code below this line
+  return (a < b);
+  // Only change code above this line
+}
+
+console.log(isLess(10, 15));
+
+
+// function exit immediately after a return
+
+/*
+Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+*/
+
+function abTest(a, b) {
+
+  if(a<0 || b<0){
+    return undefined;
+  }
+  
+    
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  console.log(abTest(2,2));
+
+
+  let count = 0;
+
+function cc(card) {
+  
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count++;
+    break;
+
+    case 7:
+    case 8:
+    case 9:
+    count+=0;
+    break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+    count--;
+    break;
+  }
+
+  if(count>0){
+    return count + " Bet";
+  }
+  else if(count <=0){
+    return count + " Hold";
+  }
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
