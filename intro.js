9edf34aad4b23b7fc67f77d63b5489ee0f2da12f
Adobyzer <adam.abed02@hotmail.com>
2023-11-02 22:29:53 -0400

    First round variable type(var,let,const and manipulation)

--- intro.js
+++ intro.js
@@ -6,3 +6,153 @@ multi line
 comment
 */
 console.log("Hello JS")
+
+/*
+list of data type : 
+
+undefined
+null
+boolean
+string ex "name"
+symbol
+bigint
+number ex 12 or 1,2
+object
+*/
+
+//declaration of a variable myName
+var myName;
+
+// Declaration
+var a;
+
+// Initialisation/Instanciation
+
+a =7;
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// assign value content a to b
+b = a 
+
+//initialisation (declaration et instanciation)
+
+var a =9;
+
+//initialisation of two string variable 
+
+var myFirstName = "Adobyzer"
+
+var myLastName = "Adbz"
+
+// variable undefined type if not initialized
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Then take data with content on it dynamically
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations in camel case
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//let use "let" doesnt allow repetition of same variable no more var right now !
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+/*const is for constant (ALL CAPS norms) so no changment allow let u can but not overwrite it!
+*/
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
+
+//now sum will value 20!
+const sum = 20 + 0;
+
+//now difference will value 12!
+const difference = 45 - 33;
+
+//now multiplicity will value  80!
+const product = 8 * 10;
+
+//now quotient will value 2!
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// myVar++ => myVar = myVar+1
+myVar++;
+
+let myVars = 11;
+
+// same for myVars-- => myVars = myVars-1
+myVars--;
+
+// creating decimal with number type
+let myDecimal = 5.7
+
+//change products to make it 5.0
+const products = 2.0 * 2.5;
+
+//change quotientz to equal 2.2
+const quotientz = 4.4 / 2.0;
+
+//reminder of the quotient ex 11%3 = 2
+const remainder = 11%3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+//compound assignment ex :a=a+12 (can be writter a+=12)
+a+=12;
+b+=9;
+c+=7;
+
+//var for letting me call again the variable hehe
+var a = 11;
+var b = 9;
+var c = 3;
+
+// same for substraction ex (a=a-6 => a-=6)
+a-=6;
+b-=15;
+c-=1;
+
+//again var for it!
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// same for multipliying
+a*=5;
+b*=3;
+c*=10;
+
+//finally for it
+var a = 48;
+var b = 108;
+var c = 33;
+
+// same for division
+a/=12;
+b/=4;
+c/=11;
+
+/*single or double quote for string but for quote in string the /
+*/
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
