1ad8a593569ee3eb5011c6bccd6ba1e537fb6f5e
Adobyzer <adam.abed02@hotmail.com>
2023-11-03 00:57:23 -0400

    functions , params and return with global or local variables
    Try adding @test package to transfer it to TS

--- intro.js
+++ intro.js
@@ -68,7 +68,7 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
-//let use "let" doesnt allow repetition of same variable no more var right now !
+//let use "let" doesnt allow repetition of same variable no more var right now ! (if i need it tho)
 
 let catName = "Oliver";
 let catSound = "Meow!";
@@ -81,7 +81,7 @@ fact = "is awesome!";
 console.log(FCC, fact); 
 
 //now sum will value 20!
-const sum = 20 + 0;
+var sum = 20 + 0;
 
 //now difference will value 12!
 const difference = 45 - 33;
@@ -301,3 +301,161 @@ const myArray = [["John", 23], ["cat", 2]];
 //Push ["dog", 3] onto the end of the myArray variable.
 
 myArray.push(["dog",3]);
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+/*
+Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+*/
+
+var removedFromMyArray = myArray.pop();
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+/*
+Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+*/
+
+let removedFromMyArray = myArray.shift();
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+//Add ["Paul", 35] to the beginning of the myArray variable using unshift()
+
+myArray.unshift(["Paul",35]);
+
+//setup
+const myList = [];
+
+//There should be at least 5 sub-arrays in the list.
+
+myList.push(["chocoolatteee",10]);
+myList.push(["chocoolatteee",10]);
+myList.push(["chocoolatteee",10]);
+myList.push(["chocoolatteee",10]);
+myList.push(["chocoolatteee",10]);
+
+
+//reusable part called functions
+
+/*
+Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function.
+*/
+
+function reusableFunction(){
+    console.log("Hi World");
+  }
+  
+  reusableFunction();
+
+
+
+  //Passing Values to Functions with Arguments
+
+/*
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.
+*/
+
+//in js no need to specify type of arguments
+function functionWithArgs(arg1,arg2){
+    console.log(arg1+arg2);
+    }
+    
+    functionWithArgs(2,13);
+
+
+    //Return a Value from a Function with Return
+
+/*
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+*/
+
+function timesFive(args1){
+    return args1*=5;
+    }
+
+
+    //Global Scope and Functions
+
+/*
+Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+
+Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
+*/
+
+let myGlobal = 10;
+
+function fun1() {
+  oopsGlobal = 5;
+}
+
+//output fonction
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+//Local Scope and Functions
+
+/*
+Declare a local variable myVar inside myLocalScope and run the tests.
+*/
+
+function myLocalScope() {
+  
+    let myVar = 2;
+      console.log('inside myLocalScope', myVar);
+    }
+    myLocalScope();
+    
+    // Run and check the console
+    // myVar is not defined outside of myLocalScope
+    console.log('outside myLocalScope', myVar);
+
+    //Global vs. Local Scope in Functions
+
+/*
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+*/
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+let outerWear ='sweater';
+  return outerWear;
+}
+
+myOutfit();
+
+
+// Understanding void fonction
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+/*
+Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+*/
+
+function addFive(){
+  sum+=5;
+}
+addThree();
+addFive();
